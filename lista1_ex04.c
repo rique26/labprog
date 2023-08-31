ed5da7691d0f37b6b409d64d2078fae290c23dfc
rique26 <142310685+rique26@users.noreply.github.com>
2023-08-31 15:58:03 -0300

    lista1_ex04.c

---
+++ lista1_ex04.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int x;
+    printf("Valor de x: ");
+    scanf("%d", &x);
+    
+    printf("\n Triplo: %d\n Quadrado: %d\n Meio: %d ", x*3, x*x, x/2);
+    
+    return 0;
+}

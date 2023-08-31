498725d4f4ed7f548fad5c8d64e624ac6ec0f80f
rique26 <142310685+rique26@users.noreply.github.com>
2023-08-31 15:54:07 -0300

    lista1_ex2.c

---
+++ lista1_ex2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    printf("Valor de x: ");
+    scanf("%d", &x);
+    
+    printf(" Em hexadecimal: %X\n",x);
+    printf(" Em octal: %o",x);
+    
+    return 0;
+}

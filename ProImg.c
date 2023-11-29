e8e0590ee490f5eeed6d0f950d1a31c17be16eee
rique26 <142310685+rique26@users.noreply.github.com>
2023-11-29 15:26:01 -0300

    ProImg.c

--- ProImg.c
+++ ProImg.c
@@ -2,104 +2,97 @@
 #include <stdlib.h>
 #include <time.h>
 
+void filtro(int **, int , int, int );
+void printMatriz(int **, int , int );
 
+int main(){
 
-void filtro(int **matOrig, int l, int c){
+    int **matOrig, l=15, c=15;
+    int tamJanela = 3;
+    srand(time(NULL));
 
-    int soma=0;
-    int result[3][3] = {0};
-    int matFil[5][5]= {0};
-
-    /*
-    //alocacao matrizFil com zeros
-    matFil = calloc(l+2, sizeof(int*));
-    for(int i=0; i<l+2; i++)
-        matFil[i] = calloc(c+2, sizeof(int));
-    */
+    matOrig = malloc(l*sizeof(int*));
+    for(int i=0; i<l;i++) 
+        *(matOrig+i) = malloc(c*sizeof(int));
     
-    //soma matFil + MatOrig
-    for(int i=1;i<=3; i++)
-        for(int k=1; k<=3; k++)
-            *(*(matFil+i)+k) += *(*(matOrig+i-1)+k-1); 
+    for(int i=0;i<l;i++)
+        for(int k=0;k<c;k++)
+            *(*(matOrig + i)+k) = rand()%256;
 
-    /*
-    //alocacao matriz result media
+    printf("Matriz Original: \n\n");
+    printMatriz(matOrig, l ,c);
+
+    filtro(matOrig, l, c, tamJanela);
+
+}       
+
+void filtro(int **matOrig, int l, int c, int tamJanela){
+
+    int **result, **matCopia, soma=0, zerosAdic, acresc;
+    
+    switch (tamJanela)
+    {
+    case 3:
+        zerosAdic = 2;
+        acresc = 0;
+        break;
+    case 5:
+        zerosAdic = 4;
+        acresc = 1;
+        break;
+    case 7: 
+        zerosAdic = 6;
+        acresc = 2;
+        break;
+    case 9:
+        zerosAdic = 8;
+        acresc = 3;
+        break;
+    }
+
+    //alocacao das matrizes
+    matCopia = calloc(l+zerosAdic, sizeof(int*));     //5x5 para janela 3x3, 7x7 para janela 5x5 
+    for(int i=0; i<l+zerosAdic; i++) matCopia[i] = calloc(c+zerosAdic, sizeof(int));
+
     result = calloc(l, sizeof(int*));
-    for(int i=0; i<l; i++)
-        result[i] = calloc(c, sizeof(int));
-    */
+    for(int i=0; i<l; i++) result[i] = calloc(c, sizeof(int));
+    
+    //soma matCopia + MatOrig
+    for(int i=1;i<=l; i++)
+        for(int k=1; k<=c; k++)
+            *(*(matCopia+i+acresc)+k+acresc) += *(*(matOrig+i-1)+k-1); 
 
-   
-    printf("matfil somada: \n");
-    for(int i=0;i<l+2;i++){
-        for(int k=0;k<c+2;k++)
-            printf("%2d ", *(*(matFil+i)+k));
-        printf("\n");
+    printf("Copia da matriz extendida: \n\n");
+    for(int i=0;i<l+zerosAdic;i++){
+        for(int k=0;k<c+zerosAdic;k++)
+            printf("%3d  ", *(*(matCopia+i)+k));
+        printf("\n\n");
     }
     
-   
     //filtro
     for(int i=1; i<=l; i++){
         for(int k=1; k<=c; k++){
             soma = 0;
-            for(int x=-1; x<=1; x++){
-                for(int y=-1; y<=1; y++)
-                    soma += matFil[i+x][k+y]; //soma += *(*(matFil+i+x)+k+y);
-                
-            } 
-            //printf("%d ", soma);       
-            result[i-1][k-1] = soma/9; //*(*(result+i)+k) = soma/9;
+            for(int x=-1; x<=tamJanela-2; x++){
+                for(int y=-1; y<=tamJanela-2; y++)
+                    soma += *(*(matCopia+i+x)+k+y);
+            }*(*(result+i-1)+k-1) = soma/9;
         }
     }
       
-    //printf("111111");
-
-    printf("result : \n");
+    printf("\n\nMatriz com filtro: \n\n");
     for(int i=0;i<l;i++){
         for(int k=0;k<c;k++)
-            printf("%2d ", result[i][k]);
-            //printf("%2d ", *(*(result+i)+k));
-        printf("\n");
-    }
-    
-    
+            printf("%5d  ", *(*(result+i)+k));
+        printf("\n\n");
+    } 
 }
 
+void printMatriz(int **vet, int l, int c){
 
-
-int main(){
-
-    int **matOrig;
-    int l=3, c=3;
-    srand(time(NULL));
-
-
-
-    //alocacao dinamica
-    matOrig = malloc(l*sizeof(int*));
-    for(int i=0; i<c;i++) 
-        matOrig[i] = malloc(c*sizeof(int));
-    
-    //gerar aleatorios
-    for(int i=0;i<l;i++)
-        for(int k=0;k<c;k++)
-            *(*(matOrig + i)+k) = rand()%10;
-
-    //print matriz aleatoria
     for(int i=0;i<l;i++){
         for(int k=0;k<c;k++)
-            printf("%2d ", *(*(matOrig+i)+k));
-        printf("\n");
-    }
-    printf("\n\n");
-    
-
-    //////////////////////////////////////////
-
-    filtro(matOrig, l, c);
-
- 
-    
-}       
-
-
+            printf("%3d  ", *(*(vet+i)+k));
+        printf("\n\n");
+    }printf("\n\n");
+}
